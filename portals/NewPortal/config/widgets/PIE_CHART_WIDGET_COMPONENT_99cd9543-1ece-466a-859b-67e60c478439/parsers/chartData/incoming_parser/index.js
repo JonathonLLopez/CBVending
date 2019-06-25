d514318da73791cef4ce15dfce0a107e2a8f3082
Jonathon Lopez <jonathonllopez@gmail.com>
2019-06-25 11:54:20 -0500

    Asset created by JonathonLLopez

---
+++ portals/NewPortal/config/widgets/PIE_CHART_WIDGET_COMPONENT_99cd9543-1ece-466a-859b-67e60c478439/parsers/chartData/incoming_parser/index.js
@@ -0,0 +1,6 @@
+/**
+* @param {context} ctx - Object containing data and model information for this item.
+*/
+parser = (ctx) => {
+  return [{"name": "Page A", "data": 300},{"name": "Page B", "data": 400},{"name": "Page C", "data": 500},{"name": "Page D", "data": 600}];
+}
