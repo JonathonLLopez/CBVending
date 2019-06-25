f55e680070bf20b18548fc8280b4996ee1a91bd0
Jonathon Lopez <jonathonllopez@gmail.com>
2019-06-25 11:53:06 -0500

    Asset created by JonathonLLopez

---
+++ code/libraries/VendingMachineInventoryLib/VendingMachineInventoryLib.js
@@ -0,0 +1,42 @@
+var VendingMachineInventory = {};
+VendingMachineInventory.getBinInventory = function (machineID, binNum, quantity, callback) {
+    log("VendingMachineInventoryLib-getBinInventory");
+    var q = ClearBlade.Query({ collectionName: "Vending_Machine_Inventory" });
+    q.columns(["quantity"]);
+    q.equalTo("vending_machine_id", machineID);
+    q.equalTo("slot", binNum);
+    q.fetch(function (err, data) {
+        if (err) {
+            log("VendingMachineInventoryLib-getBinInventory:Error executing fetch" + JSON.stringify(err));
+            callback(err, data);
+        } else {
+            log("VendingMachineInventoryLib-getBinInventory:Row retrieved" + JSON.stringify(data));
+            callback(err, { "quantity": data.DATA[0].quantity });
+        }
+    });
+};
+
+VendingMachineInventory.updateBinInventory = function (machineID, binNum, quantity, callback) {
+    log("VendingMachineInventoryLib-updateBinInventory");
+    this.getBinInventory(machineID, binNum, quantity, function(err, data){
+        if (err) {
+            log("VendingMachineInventoryLib-updateBinInventory:Error executing fetch" + JSON.stringify(err));
+            callback(data);
+        } else {
+            log("VendingMachineInventoryLib-updateBinInventory:Row retrieved" + JSON.stringify(data));
+            var updateQuantity = parseInt(data.quantity) + parseInt(quantity);
+            var q = ClearBlade.Query({ collectionName: "Vending_Machine_Inventory" });
+            q.columns(["quantity"]);
+            q.equalTo("vending_machine_id", machineID);
+            q.equalTo("slot", binNum);
+            var updateRow = {
+                "quantity": updateQuantity
+            };
+            q.update(updateRow, function (err, data) {
+                log("VendingMachineInventoryLib-updateBinInventory:err = " + JSON.stringify(err));
+                log("VendingMachineInventoryLib-updateBinInventory:data = " + JSON.stringify(data));
+                callback(err, { "updatedQuantity": updateQuantity });
+            });
+        }
+    });
+};
